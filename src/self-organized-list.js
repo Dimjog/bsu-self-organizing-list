e3a54cae4314d23fb4100d0df23898ab8093d852
Dimjog <36519165+Dimjog@users.noreply.github.com>
2018-02-19 21:13:27 +0300

    Update self-organized-list.js

--- src/self-organized-list.js
+++ src/self-organized-list.js
@@ -66,35 +66,7 @@ var nnode = this.head;
     }
 
     removeAt(index) {
-	if(index<0&&index>=this.length)
-            return;
-
-        var a = this.head;
-        if(index==0){
-            this.head=this.head.next;
-
-            if(this.length>1) {
-                this.head.prev = null;
-            }
-            else {
-                this.tail = null;
-            }
-        }else
-            if(index==this.length-1){
-                this.tail=this.tail.prev;
-                this.tail.next=null;
-            }
-            else {
-                for (var i = 0; i < index; i++) {
-                    a = a.next;
-                }
-
-                var before = a.prev;
-                var after = a.next;
-                before.next = after;
-                after.prev = before;
-            }
-        this.length--;
+	
     }
 
     moveToFront(node) {
@@ -120,7 +92,19 @@ var nnode = this.head;
     }
 
     reorganize(data) {
-	
+	if(this.length==0)
+		return false;
+	var nnode = this.node;
+	for(var i=0;i<this.length;i++)
+	{
+		if(nnode.data == data)
+			{
+				this.moveToFront(nnode);
+				return true;
+			}
+		nnode = nnode.next;
+	}
+	return false;
     }
 
 }
